a3230b0aebb4ff76434804c4dd5558bfedec53c1
AbelTarazona <abel.tarazona@urp.edu.pe>
2023-04-15 13:52:58 -0500

    feat: add equatable to cat weight model

--- packages/cat_api/lib/src/models/weight_model.dart
+++ packages/cat_api/lib/src/models/weight_model.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'weight_model.g.dart';
@@ -8,16 +9,29 @@ part 'weight_model.g.dart';
 @JsonSerializable(
   fieldRename: FieldRename.snake,
 )
-class WeightModel {
+class WeightModel extends Equatable {
   /// {@macro rocket}
-  WeightModel({
+  const WeightModel({
     required this.imperial,
     required this.metric,
   });
 
   /// Imperial size.
-  String imperial;
+  final String imperial;
 
   /// Metric size.
-  String metric;
+  final String metric;
+
+  @override
+  List<Object?> get props => [imperial, metric];
+
+  /// Converts a JSON [Map] into a [WeightModel] instance.
+  static WeightModel fromJson(Map<String, dynamic> json) =>
+      _$WeightModelFromJson(json);
+
+  /// Converts this [WeightModel] instance into a JSON [Map].
+  Map<String, dynamic> toJson() => _$WeightModelToJson(this);
+
+  @override
+  String toString() => 'WeightModel{imperial: $imperial, metric: $metric}';
 }
