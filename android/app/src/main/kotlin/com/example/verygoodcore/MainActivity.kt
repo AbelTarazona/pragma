c03bd92a8ff3f114216f19c417a23810390d20f6
Abel Tarazona <abeljosetr15@gmail.com>
2023-04-15 12:17:34 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.pragma_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:pragma_challenge/app/app.dart';
+import 'package:pragma_challenge/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
