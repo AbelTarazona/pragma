3b0436d52a49f327f7bc07e6b396693169ae8ed0
AbelTarazona <abel.tarazona@urp.edu.pe>
2023-04-15 23:00:08 -0500

    feat: add detail logic

--- lib/detail/view/detail_view.dart
+++ lib/detail/view/detail_view.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cat_repository/cat_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:pragma_challenge/utils/extensions/sizebox.dart';
 
 class DetailView extends StatelessWidget {
   const DetailView({super.key, required this.cat});
@@ -25,6 +26,51 @@ class DetailView extends StatelessWidget {
                   CircularProgressIndicator(value: downloadProgress.progress),
             ),
             errorWidget: (context, url, error) => const Icon(Icons.error),
+          ),
+        ),
+        Expanded(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(cat.description, style: const TextStyle(fontSize: 16)),
+                8.0.heightBox,
+                const Divider(),
+                8.0.heightBox,
+                const Text(
+                  'Country',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                3.0.heightBox,
+                Text(cat.origin, style: const TextStyle(fontSize: 16)),
+                16.0.heightBox,
+                const Text(
+                  'Intelligence',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                3.0.heightBox,
+                Text(cat.intelligence.toString(),
+                    style: const TextStyle(fontSize: 16)),
+                16.0.heightBox,
+                const Text(
+                  'Adaptability',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                3.0.heightBox,
+                Text(cat.adaptability.toString(),
+                    style: const TextStyle(fontSize: 16)),
+                16.0.heightBox,
+                const Text(
+                  'Life span',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                3.0.heightBox,
+                Text('${cat.lifeSpan} years',
+                    style: const TextStyle(fontSize: 16)),
+              ],
+            ),
           ),
         )
       ],
