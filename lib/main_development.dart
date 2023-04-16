b423bd0b3886c30c17c3c039322fdbebfa6cf28a
AbelTarazona <abel.tarazona@urp.edu.pe>
2023-04-15 19:07:30 -0500

    feat: add portrait mode lock

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,11 +1,17 @@
 import 'package:cat_repository/cat_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:pragma_challenge/app/app.dart';
 import 'package:pragma_challenge/bootstrap.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   await dotenv.load(fileName: 'assets/.env');
 
